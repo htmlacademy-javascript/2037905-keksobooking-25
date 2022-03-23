3b12fd160446c91d8503e83f7e4deab184b37a24
unknown <snegvlico@gmail.com>
2022-03-23 12:34:13 +0300

    Отрисуй меня полностью. Часть 1.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
-const getRandomInt = (min, max) =>{
-    if (min >= 0 && max > min) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-  }
-
-  const getRandomFloat = (min, max, decimals = 2) =>{
-    if(min >= 0 && max > min) {
-      return parseFloat((Math.random() * (max - min) + min).toFixed(decimals));
-    }
-  }
-  getRandomInt(0,100);
-  getRandomFloat(0,200);
+import {arrayOfAdverts, ADVERTS_NUMBER} from './data.js';
+import {drawAd} from './markup-generation.js';
+arrayOfAdverts(ADVERTS_NUMBER);
+drawAd();
