694cca1645e47d27626ae47dd7df342cdc941fa3
unknown <snegvlico@gmail.com>
2022-04-01 20:45:20 +0300

    Отрисуй меня полностью 2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 import {arrayOfAdverts, ADVERTS_NUMBER} from './data.js';
 import {drawAd} from './markup-generation.js';
+import {getActiveState, getDisactiveState} from './map-form.js';
 arrayOfAdverts(ADVERTS_NUMBER);
 drawAd();
+
+getDisactiveState();
+setTimeout(getActiveState, 2000);
